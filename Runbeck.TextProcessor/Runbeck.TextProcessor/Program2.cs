db93c50544ff252573b14f768a82c9b3add6782d
Abdul Habib <abdulalimhabib@gmail.com>
2019-04-08 21:53:34 -0700

    Initial commit

---
+++ Runbeck.TextProcessor/Runbeck.TextProcessor/Program2.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Runbeck.TextProcessor
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            Console.WriteLine("Welcome to Runbeck Text Processor!");
+
+            while (isRunning)
+            {
+                Console.WriteLine("Please select an option: " + Environment.NewLine
+                    + "1- Start the Text Processor tool" + Environment.NewLine
+                    + "2- Quit application");
+
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        try
+                        {
+                            var textProcessor = new TextProcessor(
+                                GetUserInput("What is the full path of the file to process?"),
+                                GetUserInput("What is the file fomat? (c for csv or t for tsv)"),
+                                GetUserInput("How many fields should each record contain?"));
+
+                            textProcessor.Run();
+
+                            Console.WriteLine("SUCCESS!!");
+
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(string.Format("Failed to process file. {0}", exception.Message));
+                        }
+                        break;
+                    case "2":
+                        isRunning = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option entered.");
+                        break;
+                }
+
+                Console.WriteLine();
+
+            }
+        }
+
+        private static string GetUserInput(string instructions)
+        {
+            while (true)
+            {
+                Console.WriteLine(instructions);
+
+                string response = Console.ReadLine();
+
+                if (!string.IsNullOrWhiteSpace(response))
+                {
+                    return response;
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a response.");
+                }
+            }
+        }
+    }
+
+    class TextProcessor
+    {
+        #region " Enums "
+        private enum OutputType
+        {
+            Correct, Incorrect
+        }
+        #endregion
+
+        #region " Constructors "
+        public TextProcessor(string inputFilePath, string inputDelimiter, string inputFieldsCount)
+        {
+            InitializeFilePath(inputFilePath);
+            InitializeDelimiter(inputDelimiter);
+            InitializeFieldsCount(inputFieldsCount);
+        }
+        #endregion
+
+        #region " Properties "
+        public string InputFilePath { get; private set; }
+        public char Delimiter { get; private set; }
+        public int FieldsCount { get; private set; }
+        #endregion
+
+        #region " Run "
+        internal void Run()
+        {
+            try
+            {
+                if (TryGetFile(InputFilePath, out string[] fileContent))
+                {
+                    var correctFormatBuilder = new StringBuilder();
+                    var incorrectFormatBuilder = new StringBuilder();
+
+                    // skipping the first line as it contains the headers which aren't relevant
+                    fileContent.Skip(1).ToList().ForEach(a =>
+                    {
+                        if (a.Split(Delimiter).Length == FieldsCount)
+                        {
+                            correctFormatBuilder.AppendLine(a);
+                        }
+                        else
+                        {
+                            incorrectFormatBuilder.AppendLine(a);
+                        }
+                    });
+
+                    CreateOutputFile(InputFilePath, correctFormatBuilder.ToString(), OutputType.Correct);
+                    CreateOutputFile(InputFilePath, incorrectFormatBuilder.ToString(), OutputType.Incorrect);
+                }
+                else
+                {
+                    throw new Exception("Error: failed to read the supplied input file.");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        #endregion
+
+        #region " Initializers "
+        private void InitializeFieldsCount(string inputFieldsCount)
+        {
+            if (int.TryParse(inputFieldsCount.Trim(), out int fieldsCount))
+            {
+                FieldsCount = fieldsCount;
+            }
+            else
+            {
+                throw new Exception("Invalid field count entered.");
+            }
+        }
+
+        private void InitializeDelimiter(string inputDelimiter)
+        {
+            switch (inputDelimiter.Trim().ToLower())
+            {
+                case "c":
+                    Delimiter = ',';
+                    break;
+                case "t":
+                    Delimiter = '\t';
+                    break;
+                default:
+                    throw new Exception("Invalid delimiter entered.");
+            }
+        }
+
+        private void InitializeFilePath(string inputFilePath)
+        {
+            try
+            {
+                if (!IsValidPath(inputFilePath))
+                {
+                    throw new Exception("Input file path entered is invalid.");
+                }
+                if (!System.IO.File.Exists(inputFilePath))
+                {
+                    throw new Exception("Input file entered does not exist.");
+                }
+                else
+                {
+                    InputFilePath = inputFilePath;
+                }
+            }
+            catch (Exception exception)
+            {
+                throw new Exception(string.Format("Failed to initialize File Path. {0}", exception.Message));
+            }
+        }
+        #endregion
+
+        #region " Functions "
+        
+        private void ValidateParameters()
+        {
+            try
+            {
+                ValidatePath(InputFilePath);
+                ValidateFieldsCount(this.FieldsCount);
+            }
+            catch (Exception exception)
+            {
+                throw new Exception("Invalid parameter detected:" + exception.Message);
+            }
+        }
+        private void ValidatePath(string path)
+        {
+            try
+            {
+                System.IO.Path.GetFullPath(path);
+            }
+            catch (Exception)
+            {
+                throw new Exception("Invalid file path entered.");
+            }
+        }
+        private void ValidateFieldsCount(int fieldsCount)
+        {
+            if (fieldsCount == 0)
+            {
+                throw new Exception("Invalid field count entered. Field count must be a numeric value greater than 0.");
+            }
+        }
+        private void CreateOutputFile(string basePath, string content, OutputType outputType)
+        {
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                try
+                {
+                    string outputFileName = outputType == OutputType.Correct
+                        ? basePath + ".correct"
+                        : basePath + ".incorrect";
+
+                    System.IO.File.WriteAllText(outputFileName, content);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Failed to create an output file");
+                }
+            }
+        }
+        private bool TryGetFile(string path, out string[] fileContent)
+        {
+            fileContent = null;
+            try
+            {
+                fileContent = System.IO.File.ReadAllLines(path);
+                return fileContent.Any();
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        private bool IsValidPath(string path)
+        {
+            try
+            {
+                return System.IO.File.Exists(path);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+        #endregion
+    }
+}
